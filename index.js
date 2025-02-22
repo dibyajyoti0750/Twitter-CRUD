4cb31f9c13bd6537c0da475a32fc090a9f87d70a
Dibyajyoti Pramanick <dibyajyotipramanick0750@gmail.com>
2025-02-22 12:35:12 +0530

    Update PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const { name } = require("ejs");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -146,6 +146,6 @@ app.delete("/home/:id", (req, res) => {
   res.redirect("/home");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
